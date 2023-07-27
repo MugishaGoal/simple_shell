00e364d7740495debc36467af444b5d77f84ac2d
Mugisha Goal <degaullemugisha@gmail.com>
2023-07-26 20:21:15 -0700

    shows information of the builtin commands

---
+++ builtin_helper.c
@@ -0,0 +1,101 @@
+#include "shell.h"
+
+void helper_all(void);
+void helper_alias(void);
+void helper_cd(void);
+void helper_exit(void);
+void helper_help(void);
+
+/**
+ * helper_all - Shows the existing builtin shell commands.
+ */
+void helper_all(void)
+{
+	char *msg = "Shell\nThe shell commands are defined inside.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "Input 'help' to check the list.\nInput 'help name' to find out ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "check more about the function 'name'.\n\n  alias   \t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "alias [NAME[='VALUE'] ...]\n  cd    \tcd   ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "[DIRECTORY]\n  exit    \texit [STATUS]\n  env     \tenv";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n  setenv  \tsetenv [VARIABLE] [VALUE]\n  unsetenv\t";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "unsetenv [VARIABLE]\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * helper_alias - Shows information of the shell builtin command 'alias'.
+ */
+void helper_alias(void)
+{
+	char *msg = "alias: alias [NAME[='VALUE'] ...]\n\tHandles aliases.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n\talias: Prints a list of all aliases, one per line, in ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "the format NAME='VALUE'.\n\talias name [name2 ...]:prints";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " the aliases name, name2, etc. one per line, in the ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "form NAME='VALUE'.\n\talias NAME='VALUE' [...]: Defines";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " an alias for each NAME whose VALUE is given. If NAME ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "is already an alias, replace its value with VALUE.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * helper_cd - Shows information of the shell builtin command 'cd'.
+ */
+void helper_cd(void)
+{
+	char *msg = "cd: cd [DIRECTORY]\n\tChanges the current directory of the";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " process to DIRECTORY.\n\n\tIf no argument is given, the ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "command is interpreted as cd $HOME. If the argument '-' is";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " given, the command is interpreted as cd $OLDPWD.\n\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\tThe environment variables PWD and OLDPWD are updated ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "after changing the directory.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * helper_exit - Shows information of the shell builtin command 'exit'.
+ */
+void helper_exit(void)
+{
+	char *msg = "exit: exit [STATUS]\n\tExits the shell.\n\n\tThe ";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "STATUS argument is the integer used to exit the shell.";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " If no argument is given, the command is interpreted as";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " exit 0.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
+
+/**
+ * helper_help - Shows information of the shell builtin command 'help'.
+ */
+void helper_help(void)
+{
+	char *msg = "help: help\n\tCheck all the Shell builtin commands.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "\n      help [BUILTIN NAME]\n\tCheck particular information about every ";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "builtin command.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
